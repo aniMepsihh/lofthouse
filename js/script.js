c13f271ba889e42df285d707f9dc873718a5ee9f
aniMepsihh <112304266+aniMepsihh@users.noreply.github.com>
2025-02-22 16:30:47 +0500

    небольшие правки

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const nav = document.querySelector('.header__top-row');
 navBtn.onclick = function () {
     navIcon.classList.toggle('nav-icon--active');
     nav.classList.toggle('header__top-row--mobile');
-    document.body.classList.toggle('no-scroll');
 }
 
 /* Phone Mask */
